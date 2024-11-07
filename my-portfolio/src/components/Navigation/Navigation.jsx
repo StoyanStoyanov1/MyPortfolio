5344d3f86bf274c9c441a1e2dcf71b6a450c84a7
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-07 14:03:25 +0100

    translate buttons

--- my-portfolio/src/components/Navigation/Navigation.jsx
+++ my-portfolio/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,12 @@
 import 'flag-icons/css/flag-icons.min.css';
-
+import { useLanguage } from '../../context/LanguageContext';
 import { Link } from 'react-router-dom'
 
+import translateButtons from '../../utils/translator/translateButtons';
+
 export default function Navigation() {
+    const [language, setLanguage] = useLanguage();
+
     return (
         <header>
             <button className='flags'>
@@ -11,14 +15,14 @@ export default function Navigation() {
             <nav>
                
                 <div className='flags'>
-                    <span class="fi fi-de"> </span>
-                    <span class="fi fi-gb"> </span>
-                    <span class="fi fi-bg"> </span>  
+                    <span class="fi fi-de" onClick={() => setLanguage('de')} > </span>
+                    <span class="fi fi-gb" onClick={() => setLanguage('en')}> </span>
+                    <span class="fi fi-bg" onClick={() => setLanguage('bg')}> </span>  
                 </div>
                 <div className='nav-buttons'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/'>Certificates</Link>
-                    <Link to='/'>Projects</Link>
+                    <Link to='/'>{translateButtons.aboutMe[language]}</Link>
+                    <Link to='/'>{translateButtons.certificates[language]}</Link>
+                    <Link to='/'>{translateButtons.projects[language]}</Link>
                 </div>
             
             </nav>
---
+++ my-portfolio/src/utils/translator/translateButtons.js
@@ -0,0 +1,19 @@
+const translateButtons = {
+    aboutMe: {
+        en: 'About Me',
+        de: 'Über mich',
+        bg: 'За мен',
+    },
+    certificates: {
+        en: 'Certificates',
+        de: 'Zertifikate',
+        bg: 'Сертификати',
+    },
+    projects: {
+        en: 'Projects',
+        de: 'Projecte',
+        bg: 'Проекти',
+    }
+};
+
+export default translateButtons;
