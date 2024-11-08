9b160f6c7cb8162ff53899b6c5748e9efceac96d
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-08 18:35:50 +0100

    update

--- my-portfolio/src/components/certificates/CertificateDetails.jsx
+++ my-portfolio/src/components/certificates/CertificateDetails.jsx
@@ -4,13 +4,15 @@ export default function CertificateDetails ({data}){
     const [showInfo, setShowInfo] = useState(false);
 
     return (
-        <div className='certificateDetails' onClick={() => setShowInfo(!showInfo)} >
-            <h1>Python Basic</h1>
+        <div className='certificateDetails' >
+            <div className='certificateTitle' onClick={() => setShowInfo(!showInfo)}> 
+                <h1>Python Basic</h1>
+            </div>
 
             
-                <div className={`certificateInfo ${showInfo ? 'showInfo': ''}`} > 
-                    <p>Това е информацията, която се показва при натискане на бутона!</p>
-                </div>
+            <div className={`certificateInfo ${showInfo ? 'showInfo': ''}`} > 
+                <p>Това е информацията, която се показва при натискане на бутона!</p>
+            </div>
            
         </div>
     )
