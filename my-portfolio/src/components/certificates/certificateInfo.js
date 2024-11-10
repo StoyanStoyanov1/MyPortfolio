5a0507f6f323e684fe52b0cd35a7c8f5cafcc9ea
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-10 10:20:53 +0100

    python advanced to certificates

--- my-portfolio/src/components/certificates/certificateInfo.js
+++ my-portfolio/src/components/certificates/certificateInfo.js
@@ -1,6 +1,15 @@
 import translateCertifictes from "../../utils/translator/translateCertificates";
 
 const certificateInfo = [
+    {title: 'Python Advanced',
+        certificateImage: 'Python-Advanced.jfif',
+        zoomImage: 'Python-Advanced-More.jpeg',
+        kurs: (language) => `Soft Uni - ${translateCertifictes.country[language]}`,
+        date: '06.2023 - 08.2023',
+        info: (language) => translateCertifictes.pythonAdvancedInfo[language],
+        grade: (language) => `${translateCertifictes.grade[language]}: ${translateCertifictes.excelent[language]}`,
+        gitHub: 'https://github.com/StoyanStoyanov1/Python-Advanced'
+       }, 
     {title: 'Python Fundamentals',
         certificateImage: 'Python-Fundamentals.jfif',
         zoomImage: 'Python-Fundamentals-More.jpeg',
@@ -8,7 +17,7 @@ const certificateInfo = [
         date: '03.2023 - 06.2023',
         info: (language) => translateCertifictes.pythonFundamentalsInfo[language],
         grade: (language) => `${translateCertifictes.grade[language]}: ${translateCertifictes.excelent[language]}`,
-        gitHub: 'https://github.com/StoyanStoyanov1/SoftUni-Fundamental-Exercises'
+        gitHub: 'https://github.com/StoyanStoyanov1/Python-Fundamental'
        },   
     {title: 'Python Basic',
      certificateImage: 'Python-Basic.jfif',
