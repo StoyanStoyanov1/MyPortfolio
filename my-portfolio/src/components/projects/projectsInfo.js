ed5d16c3dff55b6ecc14f258b911e0c763bdf324
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-11 09:38:46 +0100

    myFavorites to projects

--- my-portfolio/src/components/projects/projectsInfo.js
+++ my-portfolio/src/components/projects/projectsInfo.js
@@ -1,5 +1,34 @@
 import translateProjects from "../../utils/translator/translateProjects";
-const projectsInfo = [
+const projectsInfo = [ {
+    title: 'MyFavorites',
+    image: 'MyFavorites.png',
+    zoomImage: 'MyFavorites.png',
+    info: (language) => translateProjects.myFavorites[language],
+    gitHub: 'https://github.com/StoyanStoyanov1/MyFavorites',
+    deploy: 'https://myfavorites.onrender.com',
+    technologies: [
+        {
+            type: 'Frontend',
+            content: 'React, HTML, CSS, JavaScript',
+        },
+        {
+            type: 'Backend',
+            content: 'Node.js, Express',
+        },
+        {
+            type: 'Database',
+            content: 'MongoDB, Mongoose',
+        },
+        {
+            type: 'Authentication',
+            content: 'JWT'
+        },
+        {
+            type: 'Styling',
+            content: 'Bootstrap, CSS modules',
+        },
+    ]
+},
     {
         title: 'CertifyMe',
         image: 'CertifyMe.png',
--- my-portfolio/src/utils/translator/translateProjects.js
+++ my-portfolio/src/utils/translator/translateProjects.js
@@ -3,6 +3,11 @@ const translateProjects = {
         bg: `CertifyMe е първият ми FullStack проект, разработен с помощта на React за фронтенд и Node.js за бекенд, с Mongoose за управление на база данни MongoDB. Целта на проекта е да създаде удобна платформа, където потребителите могат да се регистрират, да качват и управляват своите професионални сертификати.`,
         de: `CertifyMe ist mein erstes FullStack-Projekt, das mit React für das Frontend und Node.js für das Backend entwickelt wurde, wobei Mongoose zur Verwaltung der MongoDB-Datenbank verwendet wird. Das Ziel des Projekts ist es, eine benutzerfreundliche Plattform zu schaffen, auf der Benutzer ihre professionellen Zertifikate hochladen und verwalten können`,
         en: 'CertifyMe is my first FullStack project, developed using React for the frontend and Node.js for the backend, with Mongoose for database management of MongoDB. The project aims to provide a user-friendly platform where users can register, upload, and manage their professional certificates.',
+    },
+    myFavorites: {
+        bg: "MyFavorites е уеб приложение, което позволява на потребителите да споделят и оценяват своите любими филми, книги, подкасти и сериали. Всеки потребител може да добавя препоръки и да ги организира в списъци, които са видими за останалите, а заглавията могат да се коментират и оценяват.",
+        en: "MyFavorites is a web application that allows users to share and rate their favorite movies, books, podcasts, and series. Each user can add recommendations and organize them into lists visible to others, with titles open for comments and ratings.",
+        de: "MyFavorites ist eine Webanwendung, die es Nutzern ermöglicht, ihre Lieblingsfilme, Bücher, Podcasts und Serien zu teilen und zu bewerten. Jeder Nutzer kann Empfehlungen hinzufügen und in Listen organisieren, die für andere sichtbar sind, wobei die Titel kommentiert und bewertet werden können."
     }
 };
 
