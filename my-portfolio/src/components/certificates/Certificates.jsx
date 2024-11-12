3c01321983915801b2c844f4801eb15992d1e1ea
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-12 18:15:05 +0100

    update

--- my-portfolio/src/components/certificates/Certificates.jsx
+++ my-portfolio/src/components/certificates/Certificates.jsx
@@ -1,9 +1,16 @@
 import Details from "../details/Details";
 import certificateInfo from "./certificateInfo";
+import translateButtons from "../../utils/translator/translateButtons"
+import { useLanguage } from "../../context/LanguageContext";
 
 export default function Certificates () {
+    const [ language ] = useLanguage();
+
+    console.log(language)
+
     return (
         <div className="detailsContainer" >
+            <h1>{translateButtons.certificates[language]}</h1>
             {certificateInfo.map((certificate, index) => (
                 <Details key={index} info={certificate} />
             ))
--- my-portfolio/src/components/projects/Projects.jsx
+++ my-portfolio/src/components/projects/Projects.jsx
@@ -1,9 +1,14 @@
 import projectsInfo from "./projectsInfo";
 import Details from "../details/Details";
+import translateButtons from "../../utils/translator/translateButtons";
+import { useLanguage } from "../../context/LanguageContext";
 
 export default function Projects() {
+    const [ language ] = useLanguage();
+
     return (
         <div className="detailsContainer" >
+            <h1>{translateButtons.projects[language]}</h1>
             {projectsInfo.map((project, index) => (
                 <Details key={index} info={project} />
             ))
